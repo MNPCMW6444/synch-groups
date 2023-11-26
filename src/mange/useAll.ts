8c312b6264ac1e58bbd721e5f89bb55044264cf0
Michael Nusair <mnpcmw6444@gmail.com>
2023-11-26 13:59:25 +0200

    init

--- src/mange/useAll.ts
+++ src/mange/useAll.ts
@@ -1,4 +1,26 @@
-export default ()=>{
+import axios from 'axios';
+//import csv from 'async-csv';
 
-    return {}
+const YABA_CLIENT_FIELD = "nelson"
+
+const axiosInstance = axios.create({
+    baseURL: "https://api.synchapp.io",
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.YABAYOKEN}`,
+        "Content-Type": "application/json",
+        'X-API-Version': '1.0.0'
+    },
+});
+
+
+const getData = async ()=>{
+    const {data:users} = await axiosInstance.get("/users")
+    const {data:groups} = await axiosInstance.get("/groups/clientField",{params:{clientField:YABA_CLIENT_FIELD}})
+
+    return {users,groups}
+}
+
+
+export default ()=>{
+    return {getData}
 }
