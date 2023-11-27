5aed3f3e61c9058eda327631a4a50df683ad4232
Michael Nusair <mnpcmw6444@gmail.com>
2023-11-27 11:42:18 +0200

    init

--- src/mange/groups.ts
+++ src/mange/groups.ts
@@ -1,20 +1,50 @@
-const yaba=
+const yaba =
     {
         room25: {
+            manager: {back: ""},
+            movDirector: {back: ""},
+            fDirector: {back: ""},
+            roomOperator: {operator: ""},
+            fHigh: {back: "", front: "", operator: ""},
+            fLow: {back: "", front: "", operator: ""},
+            treasure1: {front: ""},
+            treasure2: {front: ""},
+            treasure3: {front: ""},
+            treasure5: {front: ""},
+            hStand1: {back: "", front: "", operator: ""},
+            hStand2: {back: "", front: "", operator: ""},
+            hStand4: {back: "", front: "", operator: ""},
+            stayingStand: {back: "", front: "", operator: ""},
+            doubleStayingStand: {back: "", front: "", operator: ""},
             rescueTeam: {
                 hStand: {
-                    back:"",
-                    front:"",
-                    operator:"",
+                    back: "",
+                    front: "",
+                    operator: "",
                 },
                 fStand: {
-                    back:"",
-                    front:"",
-                    operator:"",
+                    back: "",
+                    front: "",
+                    operator: "",
                 },
                 tStand: {
-                    front:"",
-                }
-            }
+                    front: "",
+                },
+                treasure4: {front: ""}
+            },
+        },
+        room12: {
+            manager: {back: ""},
+            seaDirector: {back: ""},
+            groundDirector: {back: ""},
+            bayDirector: {back: ""},
+            // sitting....
+            interA: { front: "", operator: ""},
+            interB: { front: "", operator: ""},
+            interC: { front: "", operator: ""},
+            interD: { front: "", operator: ""},
+            interE: { front: "", operator: ""},
+            interF: { front: "", operator: ""},
+            interG: { front: "", operator: ""},
         }
     }
